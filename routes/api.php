30c910f1552929d3ab9bbda3e8d6df6349163192
DmitriyV003 <vdmitriy003@gmail.com>
2024-03-15 11:24:25 +0300

    feat: project init

--- routes/api.php
+++ routes/api.php
@@ -28,32 +28,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::prefix('v1')->group(function () {
-    Route::post('form-application', [FormApplicationController::class, 'store']);
-    Route::post('media', [MediaController::class, 'store']);
-    Route::apiResource('site', SiteController::class)->only(['index']);
-    Route::apiResource('angle', AngleController::class)->only(['index']);
-
-    Route::prefix('admin')->group(function () {
-        Route::apiResource('form-type', FormTypeController::class)
-            ->only(['show', 'store', 'index', 'update', 'destroy']);
-        Route::apiResource('form-application', FormApplicationController::class)
-            ->only(['index', 'update']);
-        Route::apiResource('site', SiteController::class)
-            ->only(['show', 'store', 'update', 'destroy']);
-        Route::apiResource('size', SizeController::class)
-            ->only(['show', 'store', 'index', 'update', 'destroy']);
-        Route::apiResource('field-type', FieldTypeController::class)
-            ->only(['show', 'store', 'index', 'update', 'destroy']);
-        Route::apiResource('angle', AngleController::class)
-            ->only(['show', 'store', 'update', 'destroy']);
-        Route::apiResource('style', StyleController::class)
-            ->only(['show', 'store', 'index', 'update', 'destroy']);
-        Route::apiResource('order', OrderController::class)
-            ->only(['show', 'store', 'index', 'update']);
-        Route::apiResource('invoice', InvoiceController::class)->only(['index']);
-        Route::post('order/{order}/credit', [OrderController::class, 'createCreditInvoice']);
-        Route::post('stats', [StatsController::class, 'statsByPeriod']);
-    });
-});
